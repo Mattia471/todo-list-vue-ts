ca975937891d86ff34cc60d32ef0beec9b379d5c
Mattia471 <cucuzzamattia47@gmail.com>
2023-04-05 12:02:10 +0200

    create first component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+
+const props = defineProps<{
+  placeholder: string;
+  content: string;
+}>();
+const reactiveData = reactive({
+  content: ""
+});
+</script>
+
+<template>
+  {{reactiveData.content}}
+  <input type="text"
+         class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+         :placeholder="props.placeholder"
+         v-model="reactiveData.content">
+</template>
--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
+import BaseInput from "../components/BaseInput.vue";
 </script>
 
 <template>
-  <h1>TITOLO</h1>
+  <h1>Input Component</h1>
+  <base-input placeholder="Inserisci Testo"></base-input>
 </template>
-
-<style scoped>
-</style>
