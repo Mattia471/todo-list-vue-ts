bad825c41760525f1e4f4bdbb5cb79300bf83c62
Mattia471 <cucuzzamattia47@gmail.com>
2023-04-28 10:40:43 +0200

    add DevExtreme

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import './index.css'
 import App from './App.vue'
-
+import 'devextreme/dist/css/dx.light.css';
+import {DxChart} from "devextreme-vue";
+import {DxSeries} from "devextreme-vue/chart";
 createApp(App)
+    .component('DxChart', DxChart)
+    .component('DxSeries', DxSeries)
     .mount('#app')
