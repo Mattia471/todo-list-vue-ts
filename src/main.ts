e426b0db719cbaa6dbc0ef2f2cfa62ca0f14a4f2
Mattia471 <cucuzzamattia47@gmail.com>
2023-04-28 11:03:35 +0200

    refactor import

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,7 @@ import {createApp} from 'vue'
 import './index.css'
 import App from './App.vue'
 import 'devextreme/dist/css/dx.light.css';
-import {
-    DxColumn,
-    DxDataGrid,
-    DxGrouping,
-    DxGroupPanel,
-    DxPager,
-    DxPaging,
-    DxSearchPanel
-} from 'devextreme-vue/data-grid';
+import {DxDataGrid} from 'devextreme-vue/data-grid';
 import {DxChart} from "devextreme-vue";
 import {DxSeries} from "devextreme-vue/chart";
 
@@ -18,10 +10,4 @@ createApp(App)
     .component('DxChart', DxChart)
     .component('DxSeries', DxSeries)
     .component('DxDataGrid', DxDataGrid)
-    .component('DxColumn', DxColumn)
-    .component('DxGrouping', DxGrouping)
-    .component('DxGroupPanel', DxGroupPanel)
-    .component('DxPager', DxPager)
-    .component('DxPaging', DxPaging)
-    .component('DxSearchPanel', DxSearchPanel)
     .mount('#app')
